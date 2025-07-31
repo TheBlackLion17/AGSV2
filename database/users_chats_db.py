888184b2f96c1ef0d6c6711ef5da79e3e5fdcd19
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 17:05:08 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,13 +1,10 @@
-import motor.motor_asyncio
-import os
-from dotenv import load_dotenv
+# database/users_chats_db.py
+
+from motor.motor_asyncio import AsyncIOMotorClient
 from info import MONGO_URL
 
-
-load_dotenv()
-
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
-db = client.AutoFilterDB
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.auto_filter_db  # Use consistent DB name (lowercase, snake_case)
 
 class UserDB:
     def __init__(self):
@@ -15,32 +12,30 @@ class UserDB:
         self.chat_col = db.chats
 
     async def add_user(self, user_id: int, first_name: str):
-        user = await self.user_col.find_one({"_id": user_id})
-        if not user:
+        if not await self.user_col.find_one({"_id": user_id}):
             await self.user_col.insert_one({"_id": user_id, "first_name": first_name})
 
-    async def is_user(self, user_id: int):
+    async def is_user(self, user_id: int) -> bool:
         return await self.user_col.find_one({"_id": user_id}) is not None
 
-    async def get_all_users(self):
+    async def get_all_users(self) -> list:
         return await self.user_col.find().to_list(None)
-
-    async def add_chat(self, chat_id: int, title: str):
-        chat = await self.chat_col.find_one({"_id": chat_id})
-        if not chat:
-            await self.chat_col.insert_one({"_id": chat_id, "title": title})
-
-    async def get_all_chats(self):
-        return await self.chat_col.find().to_list(None)
-
-    async def is_chat(self, chat_id: int):
-        return await self.chat_col.find_one({"_id": chat_id}) is not None
 
     async def remove_user(self, user_id: int):
         await self.user_col.delete_one({"_id": user_id})
 
+    async def add_chat(self, chat_id: int, title: str):
+        if not await self.chat_col.find_one({"_id": chat_id}):
+            await self.chat_col.insert_one({"_id": chat_id, "title": title})
+
+    async def is_chat(self, chat_id: int) -> bool:
+        return await self.chat_col.find_one({"_id": chat_id}) is not None
+
+    async def get_all_chats(self) -> list:
+        return await self.chat_col.find().to_list(None)
+
     async def remove_chat(self, chat_id: int):
         await self.chat_col.delete_one({"_id": chat_id})
 
-
-userdb = UserDB()
+# Export instance
+db_instance = UserDB()
