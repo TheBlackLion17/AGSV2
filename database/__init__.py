82f2fd5afaca521b726f0ce1585eda49fa3fd2b3
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 17:05:14 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -5,3 +5,4 @@ from .filters_mdb import filters_db
 from .gfilters_mdb import gfilters_db
 from .la_filterdb import la_db
 from .connections_mdb import conn_db  # this will NOT give NameError anymore
+from .filters_mdb import db as filters_db
