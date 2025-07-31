467589f234e541d98ce131e01cb7df42ed61db44
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 23:27:48 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,7 @@
+# Import reusable database instances from each module
 
-from .users_chats_db import db
-from .connections_mdb import conn_db
-from .filters_mdb import filters_db
-from .gfilters_mdb import gfilters_db
-from .la_filterdb import la_db
-from .connections_mdb import conn_db  # this will NOT give NameError anymore
-from .filters_mdb import db as filters_db
+from .users_chats_db import userdb  # Instance of UsersChatsDB
+from .connections_mdb import conn_db  # Instance of connection DB class
+from .filters_mdb import filters_db  # Instance of filters DB class
+from .gfilters_mdb import gfilters_db  # Instance of global filters DB class
+from .la_filterdb import la_db  # Instance of last active filter DB class
