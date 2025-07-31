bd70969f3386976d98a6994ecbc09f81524086a7
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-08-01 03:37:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ pip install -U pip
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TheBlackLion17/AADVAN_VPS.git /AADVAN_VPS
+  git clone https://github.com/TheBlackLion17/AGSV2.git /AGSV2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AADVAN
+  git clone $UPSTREAM_REPO /AGSV2
 fi
-cd /AADVAN
+cd /AGSV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
