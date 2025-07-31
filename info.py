cd573b8d2a16f2273140df8a8b2f1dc49bf1c1ad
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-08-01 00:34:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,12 +1,15 @@
 import os
+import re
 import time
 from os import environ
+from Script import *
 
 
 
 API_ID = int(os.environ.get("API_ID", "13357171"))  # Replace with your actual API ID
 API_HASH = os.environ.get("API_HASH", "d39c4324a40a8a6b27a067f8ff2b987e")  # Replace with your actual API HASH
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8161552461:AAEIRDGMbwBDIrtjmVeD9k6DxdIf55KuN3s")  # Replace with your actual bot token
+SESSION = environ.get('SESSION', 'Media_search')
 
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "agsfilterv2_bot")
 PLUGINS = os.environ.get("PLUGINS", "plugins")  # Your plugins/modules folder name
@@ -18,6 +21,12 @@ AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", "-1001614481524"))  # Optional
 
 BOT_UPTIME  = time.time()
 
+# ============================
+# Bot Settings Configuration
+# ============================
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+
 # Thumbnail or Start Image
 START_UP_PIC = (environ.get('START_UP_PICS' ,'https://telegra.ph/file/7813a2970277d0ae265a1.jpg')).split() # List of images
 
@@ -26,11 +35,17 @@ UPDATE_CHANNEL_URL = os.environ.get("UPDATE_CHANNEL_URL", "https://t.me/AgsModsO
 SUPPORT_GROUP_URL = os.environ.get("SUPPORT_GROUP_URL", "https://t.me/+A9AvnxcFRNQ5Njc1")
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "https://t.me/+A9AvnxcFRNQ5Njc1")
 CHANNEL = os.environ.get("CHANNEL", "https://t.me/AgsModsOG")
-
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', 'https://t.me/AgsModsOG'))
 # Database 
 
-MONGO_URL = environ.get('MONGO_URL', "mongodb+srv://v2:v2@cluster0.9dbt2hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME","cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://v2:v2@cluster0.9dbt2hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME","cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://v2:v2@cluster0.9dbt2hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+
+
+
+CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla", "Telugu", "Malayalam", "Kannada", "Marathi", "Punjabi", "Bengoli", "Gujrati", "Korean", "Gujarati", "Spanish", "French", "German", "Chinese", "Arabic", "Portuguese", "Russian", "Japanese", "Odia", "Assamese", "Urdu"]
 
 # wes response configuration     
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
@@ -43,5 +58,9 @@ PREMIUM_USER_MAX_FILE_SIZE = int(os.environ.get("PREMIUM_USER_MAX_FILE_SIZE", 40
 THUMBNAIL_DIR = "./downloads/thumb/"
 DOWNLOAD_DIR = "./downloads/"
 
+MAX_B_TN = environ.get("MAX_B_TN", "5")
+MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
+
+
 # Log Channel & Developer Contact
-OWNER_ID = int(os.environ.get("OWNER_ID", "7705748477"))
+OWNERID = int(os.environ.get("OWNER_ID", "7705748477"))
