5d33af5cb9c4a1b2da0256239ac892378aa5e095
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 15:33:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,27 +1,27 @@
-# info.py
-
 import os
 
-API_ID = int(os.environ.get("API_ID", "123456"))  # Replace with your actual API ID
-API_HASH = os.environ.get("API_HASH", "your_api_hash")  # Replace with your actual API HASH
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")  # Replace with your actual bot token
+API_ID = int(os.environ.get("API_ID", "13357171"))  # Replace with your actual API ID
+API_HASH = os.environ.get("API_HASH", "d39c4324a40a8a6b27a067f8ff2b987e")  # Replace with your actual API HASH
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8161552461:AAEIRDGMbwBDIrtjmVeD9k6DxdIf55KuN3s")  # Replace with your actual bot token
 
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourBotUsername")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "agsfilterv2_bot")
 PLUGINS = os.environ.get("PLUGINS", "plugins")  # Your plugins/modules folder name
 
 # Admins and Channels
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "123456789").split()]
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001234567890"))  # Log channel ID
-AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", "-1001234567890"))  # Optional: force join
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "7705748477").split()]
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002801544620"))  # Log channel ID
+AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", "-1001614481524"))  # Optional: force join
 
 # Thumbnail or Start Image
-START_UP_PIC = os.environ.get("PICS", "https://telegra.ph/file/abcd12345.jpg").split()  # List of images
+START_UP_PIC = os.environ.get("PICS", "https://files.catbox.moe/g00ax7.jpg").split()  # List of images
 
-# Database
-MONGO_URL = environ.get('MONGO_URL', "mongodb+srv://v2:v2@cluster0.9dbt2hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # Other URLs or Settings
-UPDATE_CHANNEL_URL = os.environ.get("UPDATE_CHANNEL_URL", "https://t.me/YourUpdates")
-SUPPORT_GROUP_URL = os.environ.get("SUPPORT_GROUP_URL", "https://t.me/YourSupport")
+UPDATE_CHANNEL_URL = os.environ.get("UPDATE_CHANNEL_URL", "https://t.me/AgsModsOG")
+SUPPORT_GROUP_URL = os.environ.get("SUPPORT_GROUP_URL", "https://t.me/+A9AvnxcFRNQ5Njc1")
+
+# Database 
+
+MONGO_URL = environ.get('MONGO_URL', "mongodb+srv://v2:v2@cluster0.9dbt2hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Optional Limits
 FREE_USER_MAX_FILE_SIZE = int(os.environ.get("FREE_USER_MAX_FILE_SIZE", 2048))  # In MB
@@ -32,4 +32,4 @@ THUMBNAIL_DIR = "./downloads/thumb/"
 DOWNLOAD_DIR = "./downloads/"
 
 # Log Channel & Developer Contact
-OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7705748477"))
