98ff539f1fe18aad81cd15f15db1db1fbb3cefb5
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 20:56:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,76 +1,60 @@
-import os, math, logging, datetime, pytz, logging.config
-
+import os
+from datetime import datetime
+from pytz import timezone
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from info import *
 from aiohttp import web
-from pyrogram import Client, types
-from database.users_chats_db import db
-from database.ia_filterdb import  Media
 from route import web_server
-from typing import Union, Optional, AsyncGenerator
-from utils import temp, __repo__, __license__, __copyright__, __version__
-from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
+import pyrogram.utils
+import pyromod
 
-# Get logging configurations
-logging.config.fileConfig("logging.conf")
-logging.getLogger(__name__).setLevel(logging.INFO)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            name="Professor-Bot",
+            name="renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins")
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
         )
 
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats        
-        
         await super().start()
-        await Media.ensure_indexes()
         me = await self.get_me()
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.id = me.id
-        self.name = me.first_name
         self.mention = me.mention
-        self.username = me.username
-        self.log_channel = LOG_CHANNEL
-        self.uptime = UPTIME
-        curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
-        date = curr.strftime('%d %B, %Y')
-        tame = curr.strftime('%I:%M:%S %p')
-        logging.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        
-        try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-        
-        if bool(WEB_SUPPORT) is True:
+        self.username = me.username  
+        self.uptime = BOT_UPTIME     
+        if WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()
-            PORT = int(os.environ.get("PORT", 8000))  # Use port 8000 or env PORT
+            PORT = int(os.environ.get("PORT", 8081))  # Use port 8000 or env PORT
             await web.TCPSite(app, "0.0.0.0", PORT).start()
         print(f"{me.first_name} Is Started.....✨️")
-            
-    async def stop(self, *args):
-        await super().stop()
-        logging.info(f"Bot Is Restarting ⟳...")
-
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
-
-
+        for id in Config.ADMIN:
+            try: 
+                await self.send_message(id, f"**{me.first_name} Is Started...**")                                
+            except Exception as e:
+                print(f"Error sending message to admin {id}: {e}")
         
+        if LOG_CHANNEL:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+            except Exception as e:
+                print(f"Error sending message to LOG_CHANNEL: {e}")
+
+    async def stop(self):
+        await super().stop()
+        print(f"{self.mention} is stopped.")
+
 Bot().run()
