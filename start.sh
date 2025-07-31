f328c06ca705eec6aec52aee4876028b1ab5394e
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-31 14:24:37 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+apt update && apt upgrade -y
+
+apt install git -y           
+pip install -U pip  
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/TheBlackLion17/AGSV2.git /AGSV2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /AGSV2
+fi
+cd /AADVAN
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
