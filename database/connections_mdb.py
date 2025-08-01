7aa9a477d4d8414b58818f20c24e646fc5e779ea
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-08-01 09:28:08 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,135 +1,106 @@
-import pymongo
-
+import logging
+from motor.motor_asyncio import AsyncIOMotorClient
 from info import DATABASE_URL, DATABASE_NAME
 
-import logging
+# Logging setup
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 
-myclient = pymongo.MongoClient(DATABASE_URL)
-mydb = myclient[DATABASE_NAME]
-mycol = mydb['CONNECTION']   
+# MongoDB setup
+client = AsyncIOMotorClient(DATABASE_URL)
+db = client[DATABASE_NAME]
+collection = db['CONNECTION']
 
 
-async def add_connection(group_id, user_id):
-    query = mycol.find_one(
-        { "_id": user_id },
-        { "_id": 0, "active_group": 0 }
-    )
-    if query is not None:
-        group_ids = [x["group_id"] for x in query["group_details"]]
-        if group_id in group_ids:
-            return False
-
-    group_details = {
-        "group_id" : group_id
-    }
-
-    data = {
-        '_id': user_id,
-        'group_details' : [group_details],
-        'active_group' : group_id,
-    }
-
-    if mycol.count_documents( {"_id": user_id} ) == 0:
-        try:
-            mycol.insert_one(data)
-            return True
-        except:
-            logger.exception('Some error occured!', exc_info=True)
-
-    else:
-        try:
-            mycol.update_one(
-                {'_id': user_id},
+async def add_connection(group_id: int, user_id: int) -> bool:
+    try:
+        user = await collection.find_one({"_id": user_id})
+        if user:
+            group_ids = [x["group_id"] for x in user.get("group_details", [])]
+            if group_id in group_ids:
+                return False
+            await collection.update_one(
+                {"_id": user_id},
                 {
-                    "$push": {"group_details": group_details},
-                    "$set": {"active_group" : group_id}
+                    "$push": {"group_details": {"group_id": group_id}},
+                    "$set": {"active_group": group_id}
                 }
             )
-            return True
-        except:
-            logger.exception('Some error occured!', exc_info=True)
-
-        
-async def active_connection(user_id):
-
-    query = mycol.find_one(
-        { "_id": user_id },
-        { "_id": 0, "group_details": 0 }
-    )
-    if not query:
-        return None
-
-    group_id = query['active_group']
-    if group_id != None:
-        return int(group_id)
-    else:
-        return None
-
-
-async def all_connections(user_id):
-    query = mycol.find_one(
-        { "_id": user_id },
-        { "_id": 0, "active_group": 0 }
-    )
-    if query is not None:
-        return [x["group_id"] for x in query["group_details"]]
-    else:
-        return None
-
-
-async def if_active(user_id, group_id):
-    query = mycol.find_one(
-        { "_id": user_id },
-        { "_id": 0, "group_details": 0 }
-    )
-    return query is not None and query['active_group'] == group_id
-
-
-async def make_active(user_id, group_id):
-    update = mycol.update_one(
-        {'_id': user_id},
-        {"$set": {"active_group" : group_id}}
-    )
-    return update.modified_count != 0
-
-
-async def make_inactive(user_id):
-    update = mycol.update_one(
-        {'_id': user_id},
-        {"$set": {"active_group" : None}}
-    )
-    return update.modified_count != 0
-
-
-async def delete_connection(user_id, group_id):
-
-    try:
-        update = mycol.update_one(
-            {"_id": user_id},
-            {"$pull" : { "group_details" : {"group_id":group_id} } }
-        )
-        if update.modified_count == 0:
-            return False
-        query = mycol.find_one(
-            { "_id": user_id },
-            { "_id": 0 }
-        )
-        if len(query["group_details"]) >= 1:
-            if query['active_group'] == group_id:
-                prvs_group_id = query["group_details"][len(query["group_details"]) - 1]["group_id"]
-
-                mycol.update_one(
-                    {'_id': user_id},
-                    {"$set": {"active_group" : prvs_group_id}}
-                )
         else:
-            mycol.update_one(
-                {'_id': user_id},
-                {"$set": {"active_group" : None}}
-            )
+            await collection.insert_one({
+                "_id": user_id,
+                "group_details": [{"group_id": group_id}],
+                "active_group": group_id
+            })
         return True
     except Exception as e:
-        logger.exception(f'Some error occured! {e}', exc_info=True)
+        logger.exception("Error in add_connection: %s", e)
+        return False
+
+
+async def active_connection(user_id: int) -> int | None:
+    try:
+        user = await collection.find_one({"_id": user_id}, {"active_group": 1})
+        return int(user["active_group"]) if user and user.get("active_group") else None
+    except Exception as e:
+        logger.exception("Error in active_connection: %s", e)
+        return None
+
+
+async def all_connections(user_id: int) -> list[int] | None:
+    try:
+        user = await collection.find_one({"_id": user_id}, {"group_details": 1})
+        return [x["group_id"] for x in user.get("group_details", [])] if user else None
+    except Exception as e:
+        logger.exception("Error in all_connections: %s", e)
+        return None
+
+
+async def if_active(user_id: int, group_id: int) -> bool:
+    try:
+        user = await collection.find_one({"_id": user_id}, {"active_group": 1})
+        return user and user.get("active_group") == group_id
+    except Exception as e:
+        logger.exception("Error in if_active: %s", e)
+        return False
+
+
+async def make_active(user_id: int, group_id: int) -> bool:
+    try:
+        result = await collection.update_one({"_id": user_id}, {"$set": {"active_group": group_id}})
+        return result.modified_count > 0
+    except Exception as e:
+        logger.exception("Error in make_active: %s", e)
+        return False
+
+
+async def make_inactive(user_id: int) -> bool:
+    try:
+        result = await collection.update_one({"_id": user_id}, {"$set": {"active_group": None}})
+        return result.modified_count > 0
+    except Exception as e:
+        logger.exception("Error in make_inactive: %s", e)
+        return False
+
+
+async def delete_connection(user_id: int, group_id: int) -> bool:
+    try:
+        result = await collection.update_one(
+            {"_id": user_id},
+            {"$pull": {"group_details": {"group_id": group_id}}}
+        )
+        if result.modified_count == 0:
+            return False
+
+        user = await collection.find_one({"_id": user_id})
+        remaining_groups = user.get("group_details", [])
+        if not remaining_groups:
+            await make_inactive(user_id)
+        elif user.get("active_group") == group_id:
+            new_active = remaining_groups[-1]["group_id"]
+            await make_active(user_id, new_active)
+
+        return True
+    except Exception as e:
+        logger.exception("Error in delete_connection: %s", e)
         return False
