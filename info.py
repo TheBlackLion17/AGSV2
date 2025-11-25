03337f14d86cc12de32dbdbdf69335feabc8f9c8
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-11-25 20:30:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,6 +70,16 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+LOG_MSG = """
+<b>🔄 𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽!</b>
+AgsModsOG 
+<a href="https://t.me/AgsModsOG">@AgsModsOG</a>
+
+📅 𝖣𝖺𝗍𝖾 : <code>{}</code>  
+⏰ 𝖳𝗂𝗆𝖾 : <code>{}</code>  
+🌐 𝖳𝗂𝗆𝖾𝗓𝗈𝗇𝖾 : <code>Asia/Kolkata</code>  
+🛠️ 𝖡𝗎𝗂𝗅𝖽 𝖲𝗍𝖺𝗍𝗎𝗌 : <code>𝗏2 [ 𝖲𝗍able 😁 ]</code>
+"""
 
 #new❤️‍🔥
 LANGUAGES = ["malayalam", "", "tamil", "", "english", "", "hindi", "", "telugu", "", "kannada", "", "gujarati", "", "marathi", "", "punjabi", ""]
